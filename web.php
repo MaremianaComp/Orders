b2a275b90ab11ea77f1d9ed92e08d81630adb8a3
MaremianaComp <139053449+MaremianaComp@users.noreply.github.com>
2023-08-20 13:57:28 +0200

    Update web.php

--- web.php
+++ web.php
@@ -1,3 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
+
+Route::get('orders', [OrderController::class, 'index'])->name('orders');
+Route::get('orders/create', [OrderController::class, 'create'])->name('order.create');
+Route::post('orders', [OrderController::class, 'store'])->name('order.store');
+Route::get('orders/{order}', [OrderController::class, 'show'])->name('order.show');
+Route::patch('orders/{order}', [OrderController::class, 'update'])->name('order.update');
